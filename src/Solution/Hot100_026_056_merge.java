239371bc25f96b1b065fadc30bdfd9313d27ecfd
Yinfan <1791511291@qq.com>
2024-10-29 16:45:14 +0800

    dp

--- src/Solution/Hot100_026_056_merge.java
+++ src/Solution/Hot100_026_056_merge.java
@@ -15,9 +15,36 @@ public class Hot100_026_056_merge {
         }
         Collections.sort(unmerge);
 //        依次遍历，如果当前右端点的最大值够不到左端点。那么加入结果。
-        int[][] res = new int[n][2];
-        
-
+        List<Pair> res = new ArrayList<>();
+        int start = Integer.MIN_VALUE;
+        int end = Integer.MIN_VALUE;
+        int j = 0;
+        for (int i = 0; i < unmerge.size(); i++) {
+            int l = unmerge.get(i).first;
+            int r = unmerge.get(i).second;
+            if (j == 0){
+                start = l;
+                end = r;
+                j = 1;
+                continue;
+            }
+            if (l > end){
+//                下一组区间与上一组没有共同部分，将上一组加入结果
+                res.add(new Pair(start, end));
+                start = l;
+                end = r;
+                continue;
+            }
+            if (r > end) end = r;
+        }
+//        最后一组必定没有在循环里面加入结果
+        res.add(new Pair(start, end));
+        int[][] ans = new int[res.size()][2];
+        for (int i = 0; i < res.size(); i++) {
+            ans[i][0] = res.get(i).first;
+            ans[i][1] = res.get(i).second;
+        }
+        return ans;
     }
 }
 
---
+++ src/Solution/Hot100_029_070_climbStairs.java
@@ -0,0 +1,13 @@
+package Solution;
+
+public class Hot100_029_070_climbStairs {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
