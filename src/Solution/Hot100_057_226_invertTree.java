46ec62cd5d5f31009039c2be313ce187ad42c803
yinfan-24 <1791511291@qq.com>
2024-11-16 17:15:10 +0800

    Changes

---
+++ src/Solution/Hot100_057_226_invertTree.java
@@ -0,0 +1,14 @@
+package Solution;
+
+public class Hot100_057_226_invertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
