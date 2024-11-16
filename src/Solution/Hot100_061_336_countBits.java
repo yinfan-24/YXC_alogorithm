4ec969747230f82927e45d3c519cc4ab792abd11
yinfan-24 <1791511291@qq.com>
2024-11-16 21:32:43 +0800

    比特位计数

--- src/Solution/Hot100_061_336_countBits.java
+++ src/Solution/Hot100_061_336_countBits.java
@@ -7,8 +7,17 @@ public class Hot100_061_336_countBits {
     public int[] countBits(int n) {
         int[] res = new int[n+1];
         for (int i = 0; i <= n; i++) {
-            res[i] = Integer.bitCount(i);
+            res[i] = countOne(i);
         }
         return res;
+    }
+
+    public int countOne(int num){
+        int cnt = 0;
+        while (num != 0){
+            num &= num-1;
+            cnt ++;
+        }
+        return cnt;
     }
 }
