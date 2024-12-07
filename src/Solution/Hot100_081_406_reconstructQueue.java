bcd5dd39483206959fbb3b3e9362fd76cb36ab0f
yinfan-24 <1791511291@qq.com>
2024-12-07 16:05:14 +0800

    前k个高频元素

--- src/Solution/Hot100_081_406_reconstructQueue.java
+++ src/Solution/Hot100_081_406_reconstructQueue.java
@@ -1,5 +1,7 @@
 package Solution;
 
+import sun.java2d.pipe.AAShapePipe;
+
 import java.util.Arrays;
 
 public class Hot100_081_406_reconstructQueue {
@@ -8,13 +10,15 @@ public class Hot100_081_406_reconstructQueue {
             if (x1[0] == x2[0]) return x1[1] - x2[1];
             else return x2[0] - x1[0];
         });
+
         int i = 0;
         while (i < people.length){
             int[] temp = people[i];
-            System.arraycopy(people, temp[1], people, temp[1]+1, i - temp[1]);
+            System.arraycopy(people, temp[1], people, 1+temp[1], i-temp[1]);
             people[temp[1]] = temp;
             i++;
         }
         return people;
     }
+
 }
