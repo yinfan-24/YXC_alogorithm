fc89dab133b5c20c30cca145dc0a36e14c86a4b0
yinfan-24 <1791511291@qq.com>
2024-12-22 19:32:53 +0800

    最长递增子序列

---
+++ src/Solution/Hot100_089_300_lengthOfLIS.java
@@ -0,0 +1,18 @@
+package Solution;
+
+public class Hot100_089_300_lengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+        int res = 1;
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
