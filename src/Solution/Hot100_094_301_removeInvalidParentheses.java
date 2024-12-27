19af07cfe434c11c38422016f05bf3e0f72d57c1
yinfan-24 <1791511291@qq.com>
2024-12-27 21:57:25 +0800

    删除无效括号

--- src/Solution/Hot100_094_301_removeInvalidParentheses.java
+++ src/Solution/Hot100_094_301_removeInvalidParentheses.java
@@ -1,3 +1,4 @@
+
 package Solution;
 
 import java.util.ArrayList;
@@ -15,40 +16,38 @@ public class Hot100_094_301_removeInvalidParentheses {
         int l = 0, r = 0;
         for (char c: s.toCharArray()) {
             if (c == '(') l++;
-            else if (c == ')') r++;
+            else if (c == ')') {
+                if (l != 0) l--;
+                else r++;
+            }
         }
-        max = Math.max(l, r);
-        dfs(0, "", 0);
+        len = n-l-r; // 期望得到的长度,l和r分别代表需要消除的左右括号个数
+        int c1 = 0, c2 = 0;
+        for (char c: s.toCharArray()) {
+            if (c == '(') c1++;
+            else if(c == ')') c2++;
+        }
+        max = Math.min(c1, c2);
+
+        dfs(0, "", l, r, 0);
         return new ArrayList<>(set);
     }
 
-    void dfs(int u, String cur, int score){
-//        u 为枚举到的索引
-//        cur 为当前已经枚举得到的字符串
-//        score 为得分情况，左括号得分+1，右括号得分-1
-//        符合要求的字符串一定是得分为0的，且枚举过程中不会出现得分为负
-        if (score < 0 || score > max) return;
-        if (u == n){
-//            已经枚举到原始字符串的最后一个位置了，得到可能结果
-//            1、左右括号匹配
-//            2、当前结果串比先前的结果串，长度相等或者更长
-            if (score == 0 && cur.length() >= len){
-//                这里是为了保证候选结果是最长字串
-                if (cur.length() > len) set.clear();
-                len = cur.length();
-                set.add(cur);
-            }
-            return;
+    void dfs(int u, String cur, int l, int r, int score){
+        if (l < 0 || r < 0 || score < 0 || score > max) return;
+        if (l == 0 && r == 0){
+            if (cur.length() == len) set.add(cur);
         }
+        if (u == n) return;
         char c = s.charAt(u);
         if (c == '('){
-            dfs(u+1, cur + String.valueOf(c), score + 1);
-            dfs(u+1, cur, score);
+            dfs(u+1, cur+String.valueOf(c), l, r, score + 1);
+            dfs(u+1, cur, l-1, r, score);
         } else if (c == ')') {
-            dfs(u+1, cur + String.valueOf(c), score - 1);
-            dfs(u+1, cur, score);
+            dfs(u+1, cur+String.valueOf(c), l, r, score -1);
+            dfs(u+1, cur, l, r-1, score);
         } else {
-            dfs(u+1, cur + String.valueOf(c), score);
+            dfs(u+1, cur+String.valueOf(c), l, r, score);
         }
     }
 }
