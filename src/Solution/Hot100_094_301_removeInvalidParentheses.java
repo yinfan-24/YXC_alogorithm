1b8f0423d16d5d63bca6e82c1cb14395b1804088
yinfan-24 <1791511291@qq.com>
2024-12-28 20:18:22 +0800

    删除无效括号

--- src/Solution/Hot100_094_301_removeInvalidParentheses.java
+++ src/Solution/Hot100_094_301_removeInvalidParentheses.java
@@ -7,28 +7,27 @@ import java.util.List;
 import java.util.Set;
 
 public class Hot100_094_301_removeInvalidParentheses {
-    Set<String> set= new HashSet<>();
-    int n, max, len;
+    Set<String> set = new HashSet<>();
+    int max, n, len;
     String s;
     public List<String> removeInvalidParentheses(String _s) {
         s = _s;
         n = s.length();
         int l = 0, r = 0;
-        for (char c: s.toCharArray()) {
+        for (char c : s.toCharArray()){
             if (c == '(') l++;
             else if (c == ')') {
-                if (l != 0) l--;
+                if ( l > 0) l--;
                 else r++;
             }
         }
-        len = n-l-r; // 期望得到的长度,l和r分别代表需要消除的左右括号个数
+        len = n-l-r;
         int c1 = 0, c2 = 0;
-        for (char c: s.toCharArray()) {
+        for (char c : s.toCharArray()){
             if (c == '(') c1++;
-            else if(c == ')') c2++;
+            else if (c == ')') c2++;
         }
         max = Math.min(c1, c2);
-
         dfs(0, "", l, r, 0);
         return new ArrayList<>(set);
     }
@@ -41,13 +40,15 @@ public class Hot100_094_301_removeInvalidParentheses {
         if (u == n) return;
         char c = s.charAt(u);
         if (c == '('){
-            dfs(u+1, cur+String.valueOf(c), l, r, score + 1);
+            dfs(u+1, cur + String.valueOf(c), l, r, score + 1);
             dfs(u+1, cur, l-1, r, score);
         } else if (c == ')') {
-            dfs(u+1, cur+String.valueOf(c), l, r, score -1);
+            dfs(u+1, cur + String.valueOf(c), l, r, score -1);
             dfs(u+1, cur, l, r-1, score);
         } else {
             dfs(u+1, cur+String.valueOf(c), l, r, score);
         }
     }
+
+
 }
